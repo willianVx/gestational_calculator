f25eaf2c4f638816ac420e4abe0fd943133f6a0d
willianVx <willianvoliveiravx@gmail.com>
2020-05-06 22:00:48 -0300

    Add/google-tag-manager

--- pages/calcular-dpp/index.js
+++ pages/calcular-dpp/index.js
@@ -23,7 +23,15 @@ return(
         <meta charSet="UTF-8"></meta>
         <meta name="description" content="Calcular DPP"></meta>
         <meta name="keywords" content="calcular DPP"></meta>
-        <script data-ad-client="ca-pub-9572035864447557" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        {/* <!-- Google Tag Manager --> */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MGQPVLT');`
+        }} />
+        {/* <!-- End Google Tag Manager --> */}
       </Head>
 
       { submitForm.redirect && <LoadingComponent data={submitForm.data} param={submitForm.param} /> }
--- pages/result/index.js
+++ pages/result/index.js
@@ -29,7 +29,15 @@ const Result = () => {
                 <meta charSet="UTF-8"></meta>
                 <meta name="description" content="Resultado da calculadora"></meta>
                 <meta name="keywords" content="calculadora calculadora"></meta>
-                <script data-ad-client="ca-pub-9572035864447557" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                {/* <!-- Google Tag Manager --> */}
+                <script dangerouslySetInnerHTML={{
+                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-MGQPVLT');`
+                    }} />
+                {/* <!-- End Google Tag Manager --> */}
             </Head>
 
             <MenuIcon menuDisplay={menuDisplay} setMenuDisplay={setMenuDisplay} />
